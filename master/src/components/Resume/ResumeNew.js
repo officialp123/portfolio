72b5e04e2c176cf9f3089f0f0f80076a22bef699
Uchenna Chiagozie <96476199+officialp123@users.noreply.github.com>
2024-03-28 00:45:30 +0100

    Update ResumeNew.js

--- master/src/components/Resume/ResumeNew.js
+++ master/src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/officialp123/portfolio/master/src/Assets/chiagoziem.pdf";
+  "https://www.linkedin.com/in/chiagozie-uchenna/";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
